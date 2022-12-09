b09a88d722dc1bd64c22bbef7af6bceb50f1d887
Cédric MARTY <c.m2002@hotmail.fr>
2022-12-09 13:12:26 +0100

    confil resolution

--- C++ard game/Player.cpp
+++ C++ard game/Player.cpp
@@ -6,6 +6,11 @@
  */
 
 
-void Player::player_turn() {
+void Player::player_turn()
+{
 
+}
+void set_champion(Champion champion)
+{
+	champion = champion;
 }
--- C++ard game/Player.hpp
+++ C++ard game/Player.hpp
@@ -3,17 +3,25 @@
 #define _PLAYER_H
 
 #include <string>
+#include "Champion.hpp"
 
 class Player {
 private:
 	std::string name;
 	int money;
 	int level;
+	Champion champion;
 
 public:
 	Player()
 	{
 		name = "notDefined";
+		money = 0;
+		level = 0;
+	}
+	Player(std::string player_name)
+	{
+		name = player_name;
 		money = 0;
 		level = 0;
 	}
@@ -24,6 +32,7 @@ public:
 	}
 
 	void player_turn();
+	void set_champion(Champion champion);
 };
 
 #endif //_PLAYER_H                                                                                                                                                                                                                                                                                                                
